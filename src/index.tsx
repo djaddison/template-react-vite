90ece9e57cbbe1c7727b91c3100301e0e51cd2f1
Dave Addison <djaddison.vcs@gmail.com>
2021-09-22 18:47:39 -0400

    Reorder import so that css appears last

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.global.css"
 import { Application } from "./Application"
+import "./index.global.css"
 
 ReactDOM.render(
   <React.StrictMode>
